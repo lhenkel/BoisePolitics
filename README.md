e541e8c15f18d2f53b5b03ad6099fc29125b3e3d
Lee Henkel <lee.henkel@kng.com>
2016-11-16 16:53:52 -0700

    Initial wildly non-working files

---
+++ README.md
@@ -0,0 +1,1 @@
+# Eventually this will grab local news articles I like and post them to a forum for me. 
---
+++ load_articles.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import urllib
+html_doc = 'http://www.spokesman.com/blogs/boise/'
+r = urllib.urlopen(html_doc).read()
+soup = BeautifulSoup(r, 'html.parser')
+
+print soup.prettify()[0:1000]
+
+articles = soup.find_all('article')
+test_article = articles[2]
+test_article.find_all("a")[0]["href"]
