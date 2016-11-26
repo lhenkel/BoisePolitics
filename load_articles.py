4fe952ae6caec94e09d97d71fd94cbc04cd1cd1e
Lee Henkel <lhenkel@gmail.com>
2016-11-26 12:47:28 -0700

    Mostly working.  Grabs Betsy Russel and Idaho Statesmen local articles

--- load_articles.py
+++ load_articles.py
@@ -4,24 +4,35 @@ import ConfigParser, os
 import os.path
 import pickle
 import urllib
-#Somethign
+import praw
+import sys
 
 config = ConfigParser.RawConfigParser()
 config.read('boise.cfg')
 
-print config.get('Main', 'reddit_user')
-exit()
+def post_to_reddit(reddit_obj, title, summary, link, picture = '', style = 'self_post'):
+	
+	target_sub = 'IdahoPolitics'   ## change to 'test' if testing
+	#target_sub = 'test'  
+	
+	if style == 'self_post':
+		summary = summary + "\n\n" + '[>>Full Story](' + link + ')'
+	
+	got_error = False
+	try:
+		if style == 'self_post':
+			reddit_obj.subreddit(target_sub).submit(title, summary)					 
+		else:
+			reddit_obj.subreddit(target_sub).submit(title, url=link)					 
 
-html_doc = 'http://www.spokesman.com/blogs/boise/'
-r = urllib.urlopen(html_doc).read()
-soup = BeautifulSoup(r, 'html.parser')
+	except  (RuntimeError, TypeError, NameError, praw.exceptions.APIException) as err :
+		print("Oops! didnt work: ", err)
+		got_error = err
+		
+	return got_error
 
-#print soup.prettify()[0:1000]
-
-articles = soup.find_all('article')
-
+reddit = praw.Reddit(client_id=config.get('Main', 'client_id'), client_secret=config.get('Main', 'client_secret'), 	password=config.get('Main', 'password'), user_agent=config.get('Main', 'user_agent'), 	username=config.get('Main', 'username') )
 previous_urls_file = 'prev_urls.p'
-
 
 if os.path.isfile(previous_urls_file):
 	print "Found Pickle"
@@ -29,25 +40,70 @@ if os.path.isfile(previous_urls_file):
 else:
 	print "didnt find pickle"
 	prev_article_set = Set()
-
-
-for cur_article in articles:
-	cur_href = cur_article.find_all("a")[0]["href"]
 	
-	cur_title = cur_article.find_all("a")[0].contents[0]
-	cur_summary = cur_article.find_all("p")[0].contents[0]	
+## Loop through Betsy Russels articles and post	
+html_doc = 'http://www.spokesman.com/blogs/boise/'
+r = urllib.urlopen(html_doc).read()
+soup = BeautifulSoup(r, 'html.parser')
+articles = soup.find_all('article')
+
+count = 0	
+	
+for cur_article in articles:
+	count = count + 1
+	if count > 13:
+		break
+	
+	cur_href = cur_article.find_all("a")[0]["href"]
+	cur_title = cur_article.find_all("a")[0].contents[0].encode(sys.stdout.encoding, errors='replace')
+	cur_summary = cur_article.find_all("p")[0].contents[0].encode(sys.stdout.encoding, errors='replace')	
+	if cur_href.strip() != '' and cur_title.strip() != '':
+		cur_href = 'http://www.spokesman.com' + cur_href
+		print cur_title
+		if cur_href in prev_article_set:
+			print "already posted"
+		else:
+			print "new to me"
+			got_error = post_to_reddit(reddit, cur_title + ' [BR]', cur_summary, cur_href)
+			
+			if got_error == False:
+				prev_article_set.add(cur_href)
+			else:
+				print 'Post Error:' + got_error 
+
+		print "=================="
+		
+## Loop through Idaho statesman local politics and post	
+html_doc = 'http://www.idahostatesman.com/news/politics-government/state-politics/'
+r = urllib.urlopen(html_doc).read()
+soup = BeautifulSoup(r, 'html.parser')
+articles = soup.find_all('article')
+
+count = 0	
+	
+for cur_article in articles:
+	count = count + 1
+	if count > 3:
+		break
+	
+	cur_href = cur_article.find_all("a")[2]['href']
+	cur_title = cur_article.find_all("a")[2].contents[0].strip().encode(sys.stdout.encoding, errors='replace')
+	cur_summary = cur_article.find_all("p")[1].contents[0].strip().encode(sys.stdout.encoding, errors='replace')	
+
 	if cur_href.strip() != '' and cur_title.strip() != '':
 		print cur_title
 		if cur_href in prev_article_set:
 			print "already posted"
 		else:
 			print "new to me"
-			prev_article_set.add(cur_href)
-		print "=================="
+			got_error = post_to_reddit(reddit, cur_title + ' [IS]', cur_summary, cur_href)
+			
+			if got_error == False:
+				prev_article_set.add(cur_href)
+			else:
+				print 'Post Error:' + got_error 
+
+		print "=================="		
 	
 pickle.dump( prev_article_set, open(previous_urls_file, "wb" ) )
 	
-#test_article = articles[2]
-#test_article.find_all("a")[0]["href"]
-#title = test_article.find_all("a")[0].contents[0]
-#summary = test_article.find_all("p")[0].contents[0]
